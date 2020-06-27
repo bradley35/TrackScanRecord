1cdca2ff417d1d8ef0824b08d2289062a0adba24
Bradley <bamay@bamapp.net>
2020-06-27 17:50:53 -0400

    Initial Commit

---
+++ Track/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Track
+//
+//  Created by Bradley on 6/27/20.
+//  Copyright © 2020 Bradley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
